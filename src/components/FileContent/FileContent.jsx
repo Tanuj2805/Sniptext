ff9056932f1a0e88bbdb2300e982e33b9c65e2e1
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-11-29 22:13:31 +0530

    .

--- src/components/FileContent/FileContent.jsx
+++ src/components/FileContent/FileContent.jsx
@@ -3,12 +3,9 @@ import "./FileContent.css";
 function FileContent({ data, start, end }) {
 
    
-    const lines = data.split(/\n/);
-
-    // Slice lines based on start & end
+    const lines = data.split(/\n/);    
     const output = lines.slice(start - 1, end).join("\n"); 
-    // (start - 1 because array is 0-indexed)
-
+    
     return (
         <div className="file-content-container">
             <pre>{output}</pre>
