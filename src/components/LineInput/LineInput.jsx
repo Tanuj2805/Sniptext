0cfda51d2910a12252dd9b1a484eb013e36fd19d
Tanuj Keshattiwar <tanujkeshattiwar@gmail.com>
2025-11-30 00:15:33 +0530

    Fix input handling in LineInput component to ensure numeric values are set correctly

--- src/components/LineInput/LineInput.jsx
+++ src/components/LineInput/LineInput.jsx
@@ -4,7 +4,7 @@ function LineInput({printmsg, setdata})
     return(<span>
         <input className="line-input-box" type="number" placeholder={printmsg} onChange={(e)=>
             {
-                setdata(e.target.value);
+                setdata(Number(e.target.value));
             }
         }></input>
     </span>)
